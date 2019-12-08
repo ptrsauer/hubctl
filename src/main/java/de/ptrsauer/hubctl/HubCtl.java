a6036cb88c7f6219a8da2872ad0cb84a705c3482
Peter Sauer <peter.sauer@europace.de>
2019-12-08 21:00:58 +0100

    Help-Command does not need GitHub token to be present.

--- src/main/java/de/ptrsauer/hubctl/HubCtl.java
+++ src/main/java/de/ptrsauer/hubctl/HubCtl.java
@@ -3,11 +3,15 @@ package de.ptrsauer.hubctl;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.util.logging.Logger;
+
 @Command(name = "hubctl",
         mixinStandardHelpOptions = true,
         version = "0.0.1",
         description = "List your Issues and Pull Requests from GitHub.")
 public class HubCtl implements Runnable {
+
+    private static Logger log = Logger.getLogger(HubCtl.class.getSimpleName());
 
     private final TokenReader tokenReader = new TokenReader();
 
@@ -17,7 +21,13 @@ public class HubCtl implements Runnable {
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new HubCtl()).execute(args);
+        CommandLine commandLine = new CommandLine(new HubCtl());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        }
+        int exitCode = commandLine.execute(args);
         System.exit(exitCode);
     }
 }

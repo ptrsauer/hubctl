7d9e02f705d2bd81f449f9e8667befae1f6381da
Peter Sauer <peter.sauer@europace.de>
2019-12-08 21:50:35 +0100

    Some minor cleanups.

--- src/main/java/de/ptrsauer/hubctl/RepositoryReader.java
+++ src/main/java/de/ptrsauer/hubctl/RepositoryReader.java
@@ -1,13 +1,11 @@
 package de.ptrsauer.hubctl;
 
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 class RepositoryReader {
-    private Logger log = Logger.getLogger(this.getClass().getSimpleName());
 
-    private HubCtlFile hubCtlFile = new HubCtlFile();
+    private final HubCtlFile hubCtlFile = new HubCtlFile();
 
     public List<String> readRepositoryUrls() {
         return hubCtlFile.readHubCtlConfigFileLines().stream().skip(1).collect(Collectors.toList());
